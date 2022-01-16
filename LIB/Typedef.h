d8cbed9e2fec0f037a7a083ae93fad7c4dd28124
Khaled Ali <khaled21.ali33@gmail.com>
2022-01-16 20:27:41 +0200

    First Commit

---
+++ LIB/Typedef.h
@@ -0,0 +1,16 @@
+#ifndef DEF_H_INCLUDED
+#define DEF_H_INCLUDED
+
+typedef unsigned char 				uint8_t;
+typedef signed char 				sint8_t;
+typedef unsigned short 				uint16_t;
+typedef signed short 				sint16_t;
+typedef unsigned int			 	uint32_t;
+typedef signed int 					sint32_t;
+typedef unsigned long long 			uint64_t;
+typedef signed long long 			sint64_t;
+
+#define STD_TYPES_OK 	1
+#define STD_TYPES_NOK 	0
+#define NULL 			0
+#endif // DEF_H_INCLUDED
