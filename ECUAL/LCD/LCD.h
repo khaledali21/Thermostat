0ffb53de7c5f1defa1320137a14cf1ba5bc708c2
Khaled Ali <khaled21.ali33@gmail.com>
2022-01-17 13:12:28 +0200

    LCD Interface

---
+++ ECUAL/LCD/LCD.h
@@ -0,0 +1,25 @@
+/*
+ * LCD.h
+ *
+ * Created: 1/17/2022 12:19:20 AM
+ *  Author: Khaled Ali
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include "../../LIB/Typedef.h"
+#include "LCD_cfg.h"
+#include "../../MCAL/DIO/DIO_interface.h"
+
+
+uint8_t LCD_u8Init(void);
+
+uint8_t LCD_u8SendCommand(uint8_t command);
+
+uint8_t LCD_u8SendData(uint8_t data);
+
+uint8_t LCD_u8SendString(uint8_t *str);
+
+#endif /* LCD_H_ */
---
+++ ECUAL/LCD/LCD_cfg.h
@@ -0,0 +1,16 @@
+/*
+ * LCD_cfg.h
+ *
+ * Created: 1/17/2022 1:00:23 PM
+ *  Author: Khaled Ali
+ */ 
+
+
+#ifndef LCD_CFG_H_
+#define LCD_CFG_H_
+
+
+
+
+
+#endif /* LCD_CFG_H_ */
