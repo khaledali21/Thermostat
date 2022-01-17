5aaf634c74396e429c55267dd8a14f5f0bbcb6dd
Abdelrhman Nouby Elsawy <91381671+AbdelrhmanNouby@users.noreply.github.com>
2022-01-17 02:52:15 +0200

    Update ADC_u8GetRead function

--- MCAL/ADC/ADC.c
+++ MCAL/ADC/ADC.c
@@ -37,6 +37,7 @@ uint8_t ADC_u8GetRead(uint16_t* u16_ADCReading)
 			while( GET_BIT(ADCSRA_REG,ADIF) != 1 ){}
 			*u16_ADCReading = ADCL_REG | ( ADCH_REG << 8 );
 			CLR_BIT(ADCSRA_REG,ADIF);
+			CLR_BIT(ADCSRA_REG,ADSC);
 			u8ErrorState = STD_TYPES_OK ;
 			break ;		
 		}
@@ -64,7 +65,7 @@ uint8_t ADC_u8EnableInterrupt(void)
 	uint8_t u8ErrorState = STD_TYPES_OK ;
 	SET_BIT(SREG_REG, I_BIT);
 	SET_BIT(ADCSRA_REG,ADIE);
-	if ( GET_BIT(SREG_REG, I_BIT) == 0 || GET_BIT(ADCSRA_REG,ADIE) == 0 )
+	if ( (GET_BIT(SREG_REG, I_BIT) == 0) || (GET_BIT(ADCSRA_REG,ADIE) == 0) )
 	{
 		u8ErrorState = ADC_EnableInterrupt_Error ;
 	}
